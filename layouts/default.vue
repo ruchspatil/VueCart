a6d6ffa8b16268278650a6849cf72cd4c55d0ee7
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2024-12-15 15:07:06 +0530

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar />
+    <NuxtPage />
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
