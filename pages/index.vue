cab27f44472eac95f6924d6063bdb79a854aec7d
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2024-12-15 15:30:10 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center mt-10">
     <h1 class="text-4xl font-bold text-blue-500">Welcome to MyShop!</h1>
-    <p class="mt-4">Start building your amazing store here 🚀</p>
+    <p class="mt-4">Start building your amazing store here</p>
   </div>
 </template>
