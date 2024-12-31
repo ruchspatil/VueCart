5d28e4ed2faeb3b12fcaa64013aa9b73e190cbc4
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2024-12-31 15:32:52 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -55,7 +55,7 @@ export default {
 </script>
 
 <style>
-/* Importing Google font - Open Sans */
+
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap");
 
 
